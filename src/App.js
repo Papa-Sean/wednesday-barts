5a23a94182598b42e96904be0aa8ac6f49d0c548
Papa-Sean <smsugrue@gmail.com>
2021-02-11 19:05:07 -0500

    React from scratch

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            Hello!!! this is React Session from Scratch
+        </div>
+    );
+};
+
+export default App;
